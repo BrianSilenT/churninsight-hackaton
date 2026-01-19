960ec1d59e6a79a387b7c92052df0ddee3aab97c
Carla Clemente <carlaclefig@gmail.com>
2026-01-18 23:35:14 -0500

    config: Configurar el cliente API y el proveedor React Query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,21 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-  import { createRoot } from "react-dom/client";
-  import App from "./App";
-  import "./index.css";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
-if (import.meta.env.VITE_USE_MOCK?.toLowerCase() === "true") {
-  import("./mocks/browser").then(({ startWorker }) => startWorker());
+async function enableMocks() {
+  if (import.meta.env.VITE_USE_MOCK?.toLowerCase() === "true") {
+    const { startWorker } = await import("./mocks/browser");
+    await startWorker();
+  }
 }
 
-createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
-);
-  
+enableMocks().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  );
+});
